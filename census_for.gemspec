bf5de2b37e796b5b459b4323f0fb58b5d0ea5e26
Evan Koch <evankoch@gmail.com>
2015-12-28 22:25:53 -0500

    added better parsing with smarter_csv gem dependency, method to import csv data

--- census_for.gemspec
+++ census_for.gemspec
@@ -25,4 +25,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "pry", '~> 0'
+  spec.add_development_dependency "smarter_csv", '~> 1.1'
 end
--- lib/census_for.rb
+++ lib/census_for.rb
@@ -1,18 +1,23 @@
-module CensusFor
+class CensusFor
+  module ImportData
+    def get_csv_data
+      binding.pry
+      result = SmarterCSV.process("data/2014-census-data.csv")
+      result
+    end
+  end
 
   class County
-    binding.pry
+    include ImportData
     def population(county_and_state_abbrev)
       get_csv_data
     end
-
   end
 
   class State
+    include ImportData
     def population(state_abbrev)
       get_csv_data
     end
   end
-
-  require_relative "census_for/parser"
 end
